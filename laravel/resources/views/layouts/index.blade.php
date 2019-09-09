19ded00b3d68b21d3151f18a2d41a474277ba969
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2019-09-09 14:23:18 -0400

    ACtualizando nombre

--- laravel/resources/views/layouts/index.blade.php
+++ laravel/resources/views/layouts/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('plantilla/assets/images/favicon.png')}}">
-    <title>Gestion de asistencia de personal</title>
+    <title>Elecciones</title>
     <!-- Bootstrap Core CSS -->
     <link href="{{asset('plantilla/assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Custom CSS -->
