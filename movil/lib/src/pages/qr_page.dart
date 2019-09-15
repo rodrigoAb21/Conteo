be515df53d191a5b48c6f5efacb140b534c982b9
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2019-09-15 12:17:54 -0400

    QR Code

--- movil/lib/src/pages/qr_page.dart
+++ movil/lib/src/pages/qr_page.dart
@@ -1,24 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner/qr_scanner_overlay_shape.dart';
 
-class QrPage extends StatelessWidget {
+class QrPage extends StatefulWidget {
+  static final String routeName = 'qr_page';
+  QrPage({Key key}) : super(key: key);
 
-  static final String routeName = 'qr';
+  _QrPageState createState() => _QrPageState();
+}
 
+class _QrPageState extends State<QrPage> {
+  GlobalKey qrKey = GlobalKey();
+  var qrText = "";
+  QRViewController controller;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Preferencias'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
+        title: Text("QR SCAN"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Settings'),
-          Divider()
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: QRView(key: qrKey,overlay: QrScannerOverlayShape(
+                borderRadius: 10,
+                borderColor: Colors.red,
+                borderWidth: 10,
+                borderLength: 30,
+                cutOutSize: 300,
+              ), onQRViewCreated: _onQRViewCreate),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text('Scan Result: $qrText'),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreate(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+
+    });
   }
 }
