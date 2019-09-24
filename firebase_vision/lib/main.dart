e95f11284c47159f89804d5336e420745ec0ddbf
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2019-09-24 19:23:44 -0400

    Actualizando APK

---
+++ firebase_vision/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'src/pages/elecciones_page.dart';
+import 'src/pages/home_page.dart';
+import 'src/pages/qr_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        HomePage.routeName : ( BuildContext context ) => HomePage(),
+        ListaEleccionesPage.routeName : ( BuildContext context ) => ListaEleccionesPage(),
+        QrPage.routeName : ( BuildContext context ) => QrPage(),
+      },
+    );
+  }
+}
