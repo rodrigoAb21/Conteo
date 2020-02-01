f5cc8d8a817a06d8415892f47005ad3e21c665eb
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2020-01-31 20:21:36 -0400

    API

--- laravel-PWA/app/Http/Controllers/Api/ApiController.php
+++ laravel-PWA/app/Http/Controllers/Api/ApiController.php
@@ -18,10 +18,10 @@ class ApiController extends Controller
             ->get();
 
         foreach ($elecciones as $eleccion){
-            $eleccion->participantes = DB::table('participante')
-                ->join('participante_eleccion', 'participante.id', '=', 'participante_eleccion.participante_id')
-                ->where('participante_eleccion.eleccion_id', '=', $eleccion->id)
-                ->select('participante_eleccion.id', 'sigla')
+            $eleccion->partidos = DB::table('partido')
+                ->join('partido_eleccion', 'partido.id', '=', 'partido_eleccion.partido_id')
+                ->where('partido_eleccion.eleccion_id', '=', $eleccion->id)
+                ->select('partido_eleccion.id', 'sigla')
                 ->get();
         }
 
@@ -36,7 +36,7 @@ class ApiController extends Controller
                 try{
                     $resultado2 = new Resultado();
                     $resultado2->mesa_id = $mesa_id;
-                    $resultado2->participante_eleccion_id = $resultado['id'];
+                    $resultado2->partido_eleccion_id = $resultado['id'];
                     $resultado2->total = $resultado['total'];
                     $resultado2->save();
                 } catch (\Exception $e){
